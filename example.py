8455382b0e123238ad37d201e649614b1ad06716
Andrew Krug <andrew.m.krug@gmail.com>
2019-11-05 14:23:18 +0000

    default settings for python testing and starting from python3

--- example.py
+++ example.py
@@ -1,24 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common import keys
 
-d = webdriver.Chrome()
-cookie={
-    "name":"welcome",
-    'value':"True",
-    "expires": "",
-    'path': '/',
-    'httpOnly': False,
-    'HostOnly': False,
-    'Secure': True
-}
-# d.add_cookie(cookie_dict=cookie)
-d.get("https://automationintesting.online/#/admin")
-d.find_element_by_css_selector("#next").click()
-d.find_element_by_css_selector("#next").click()
-d.find_element_by_css_selector("#next").click()
-d.find_element_by_css_selector("#next").click()
-d.find_element_by_css_selector("#closeModal").click()
-d.find_element_by_css_selector("#username").send_keys("admin")
-d.find_element_by_css_selector("#password").send_keys("password" + keys.Keys.ENTER)
+@test
+def example_test():
+    d = webdriver.Chrome()
+    cookie={
+        "name":"welcome",
+        'value':"True",
+        "expires": "",
+        'path': '/',
+        'httpOnly': False,
+        'HostOnly': False,
+        'Secure': True
+    }
+    # d.add_cookie(cookie_dict=cookie)
+    d.get("https://automationintesting.online/#/admin")
+    d.find_element_by_css_selector("#next").click()
+    d.find_element_by_css_selector("#next").click()
+    d.find_element_by_css_selector("#next").click()
+    d.find_element_by_css_selector("#next").click()
+    d.find_element_by_css_selector("#closeModal").click()
+    d.find_element_by_css_selector("#username").send_keys("admin")
+    d.find_element_by_css_selector("#password").send_keys("password" + keys.Keys.ENTER)
 
-# d.quit()
+    # d.quit()
