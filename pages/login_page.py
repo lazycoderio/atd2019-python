a63e7a4c05244a409f7e8ab9758c64960b114f4f
andrewkrug <andrew.krug@saucelabs.com>
2019-11-07 12:13:39 +0100

    basic updates

---
+++ pages/login_page.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+
+
+class LoginPage(object):
+
+    def __init__(self, selenium):
+        self.driver = selenium
+
+    def login_as(self, username, password):
+        self.driver.find_element_by_id('user-name').send_keys(username)
+        self.driver.find_element_by_id('password').send_keys(password)
+        self.driver.find_element_by_css_selector('.btn_action').click()
+
+    def get_title(self):
+        return self.driver.title
+
+    def get_current_url(self):
+        return self.driver.current_url
+
+    def get_header_text(self):
+        return self.driver.find_element_by_id('header_container').text
+
+    def is_login_error_visible(self):
+        return self.driver.find_element_by_css_selector('.error-button').is_displayed()
+
+    def visit(self):
+        self.driver.get('https://www.saucedemo.com')
---
+++ tests/test_example.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common import keys
+
+def test_example(selenium):
+    d = webdriver.Chrome()
+
+    # d.quit()
